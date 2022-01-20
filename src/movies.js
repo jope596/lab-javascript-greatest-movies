4e996fcc43fcea763bd5ac50becdc7e815e66c21
José Pedro Pereira <ze.pereira96@gmail.com>
2022-01-20 22:50:01 +0000

    1 2 3 solved

--- src/movies.js
+++ src/movies.js
@@ -1,16 +1,47 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+function getAllDirectors(movies) {
+  const directorsArr = movies.map((film)=> {
+  return film.director;
+});
+return directorsArr;
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(movies) {
+  const stevenMovies = movies.filter((film)=> {
+    return film.director === 'Steven Spielberg' && film.genre.includes('Drama');
+  });
+  return stevenMovies.length;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(movies) {
+  if (!movies.length) {
+    return 0;
+  }
+  const average = movies.reduce((acc, currentValue)=> {
+  if (!currentValue.score) {
+    return acc + 0;
+  };
+  return acc + currentValue.score  
+  },0);
+  return Number ((average / movies.length).toFixed(2));
+}
+
+
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+function dramaMoviesScore(movies) {
+  if (!movies.length){
+    return 0;
+  }
+  const dramaMovies = movies.filter((film)=>{
+    return film.genre.includes('Drama');
+  });
+  return Number ((dramaMovies / dramaMovies.length).toFixed(2));
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear() {}
